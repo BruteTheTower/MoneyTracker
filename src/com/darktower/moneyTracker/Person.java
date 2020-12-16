e8aa4f4ef063a0eb45cc0133b9f0d7e34c52b6f5
Michael <wybral.m81@htlwienwest.at>
2020-12-16 21:46:31 +0100

    add fields to class.

--- src/com/darktower/moneyTracker/Person.java
+++ src/com/darktower/moneyTracker/Person.java
@@ -1,4 +1,12 @@
 package com.darktower.moneyTracker;
 
 public class Person {
+
+    String firstname;
+    String lastname;
+
+    public Person(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
 }
