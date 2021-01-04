e515ee4551846ece6cbff4bafb5add0e7ff2525f
Michael <wybral.m81@htlwienwest.at>
2021-01-04 12:25:21 +0100

    add fields to class.

--- src/com/darktower/moneyTracker/Account.java
+++ src/com/darktower/moneyTracker/Account.java
@@ -1,4 +1,14 @@
 package com.darktower.moneyTracker;
 
 public class Account {
+
+    private Person person;
+
+    public Account(Person person) {
+        this.person = person;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
 }
